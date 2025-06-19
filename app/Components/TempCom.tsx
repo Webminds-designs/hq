154e5fa68369d43e3190cd00ebcde53359f5ff1b
rumeshsmrr <siriwardhanarumesh@gmail.com>
2025-06-19 10:05:01 +0530

    create temp file and solder

---
+++ app/Components/TempCom.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TempCom() {
+  return <div>tempory component, remove this</div>;
+}
+
+export default TempCom;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import TempCom from "./Components/TempCom";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -11,6 +13,7 @@ export default function Home() {
       <h1 className="font-avenir font-light text-7xl">
         FONT 02 :- `font-avenir font-light`
       </h1>
+      <TempCom />
     </div>
   );
 }
